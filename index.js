778347da82e5501e683ca353aca3ec4554efa833
discombobulateme <paloma.oliveira@saucelabs.com>
2022-01-20 10:37:48 +0100

    Add validation not to rotate if still not the duty date

--- index.js
+++ index.js
@@ -7,22 +7,6 @@ import chalk from 'chalk';
 
 const team = require("./team.json");
 
-/* Get team array to be rotated */
-const teamArray = team[1].teamHistory;
-// console.log(teamArray)
-
-/* Write last onDuty and last backup to keep track */
-let lastOnDuty = team[1].teamHistory[0].name;
-let lastBackup = team[1].teamHistory[1].name;
-team[0].lastPeople = `Last onDuty: ${lastOnDuty}. Last backup: ${lastBackup}`
-
-/* Rotate team array positions */
-let rotate = teamArray.push(teamArray.shift());
-// console.log(teamArray)
-
-/* Writes the rotated value in the json array */
-team[1].teamHistory = teamArray;
-
 /* Find last date */
 const lastDate = team[0].lastDate;
 // console.log(lastDate)
@@ -32,6 +16,37 @@ team[0].lastDate = lastDate;
 let next = new Date(new Date().setDate(new Date(lastDate).getDate() + 14)).toDateString();
 // console.log(next)
 team[0].nextDate = next;
+
+/* Write last onDuty and last backup to keep track */
+let lastOnDuty = team[1].teamHistory[0].name;
+let lastBackup = team[1].teamHistory[1].name;
+team[0].lastPeople = `Last onDuty: ${lastOnDuty}. Last backup: ${lastBackup}`
+
+/* Find today to know if next is past or future */
+let GivenDate = next;
+var CurrentDate = new Date();
+GivenDate = new Date(GivenDate);
+
+if(GivenDate > CurrentDate){
+  console.log(`
+  Next duty date will be on ${chalk.yellowBright(next)}
+  On duty is: ${chalk.green(team[1].teamHistory[0].name)}
+  Backup is: ${chalk.magenta(team[1].teamHistory[1].name)}
+`)
+  process.exit()
+}
+
+/* Get team array to be rotated */
+const teamArray = team[1].teamHistory;
+// console.log(teamArray)
+
+/* Rotate team array positions */
+let rotate = teamArray.push(teamArray.shift());
+// console.log(teamArray)
+
+/* Writes the rotated value in the json array */
+team[1].teamHistory = teamArray;
+
 
 /* Check if next position works, or needs to be redone */
 
