c768dd791b4d91e1f8e3d4b681adb65bedf885c8
discombobulateme <paloma.oliveira@saucelabs.com>
2022-01-27 17:32:37 +0100

    Add simple test example

---
+++ test/index.test.js
@@ -0,0 +1,9 @@
+const { expect } = require('chai');
+
+  foo = 'bar',
+  beverages = { tea: ['chai', 'matcha', 'oolong'] };
+
+expect(foo).to.be.a('string');
+expect(foo).to.equal('bar');
+expect(foo).to.have.lengthOf(3);
+expect(beverages).to.have.property('tea').with.lengthOf(3);
