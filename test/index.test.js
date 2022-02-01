32282e202eb384be149565c4d929f8dd1dec5bd5
discombobulateme <paloma.oliveira@saucelabs.com>
2022-02-01 15:53:09 +0100

    Add tests to create notes

--- test/index.test.js
+++ test/index.test.js
@@ -7,3 +7,12 @@ expect(foo).to.be.a('string');
 expect(foo).to.equal('bar');
 expect(foo).to.have.lengthOf(3);
 expect(beverages).to.have.property('tea').with.lengthOf(3);
+
+
+/*
+*I does not return a null result
+* Add mock to what I expect some person to come back
+* make sure dates are progressing
+* check for invalid team member -> parse json to see if people exist
+* 
+*/
