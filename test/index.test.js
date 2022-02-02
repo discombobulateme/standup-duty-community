be8a9ff04eee2b487a71986f0e3391efe11ffb14
discombobulateme <paloma.oliveira@saucelabs.com>
2022-02-02 13:30:42 +0100

    Add 5 tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,18 +1,37 @@
-const { expect } = require('chai');
+const assert = require('assert');
+const expect = require('chai').expect;
+const should = require('chai').should();
 
-  foo = 'bar',
-  beverages = { tea: ['chai', 'matcha', 'oolong'] };
+const fs = require('fs');
+const team = JSON.parse(fs.readFileSync('./team.json', { encoding: 'utf-8' }));
 
-expect(foo).to.be.a('string');
-expect(foo).to.equal('bar');
-expect(foo).to.have.lengthOf(3);
-expect(beverages).to.have.property('tea').with.lengthOf(3);
+describe('Check team', function () {
+  it('should return number people in team', function () {
+    team[1].should.have.property('teamHistory').with.lengthOf(8);
+  });
 
+  it('onduty and index 0 should should be the same person', function () {
+    const onduty = team[1].teamHistory[0].name;
+    const index0 = team[0].onDuty;
+    assert.equal(onduty, index0);
+  });
+
+  it('onduty should not be the same person as backup', function () {
+    const onduty = team[1].teamHistory[0].name;
+    const backup = team[1].teamHistory[1].name;
+    onduty.should.not.equal(backup);
+  });
+
+  it('John should not exist in the team', function () {
+    let johnDoe = team[1].teamHistory.find((person) => person.name === 'John Doe');
+    should.not.exist(johnDoe);
+  });
+
+  it('should not return a null result', function () {
+    expect(team).not.to.be.null;
+  });
+});
 
 /*
-*I does not return a null result
-* Add mock to what I expect some person to come back
-* make sure dates are progressing
-* check for invalid team member -> parse json to see if people exist
-* 
-*/
+ * make sure dates are progressing
+ */
