d70e89f7e3882594ec9a774adaab09bc12f63175
discombobulateme <paloma.oliveira@saucelabs.com>
2022-02-03 12:16:33 +0100

    Update test using expect only as per boneskull code review

--- test/index.test.js
+++ test/index.test.js
@@ -1,30 +1,28 @@
-const assert = require('assert');
 const expect = require('chai').expect;
-const should = require('chai').should();
 
 const fs = require('fs');
 const team = JSON.parse(fs.readFileSync('./team.json', { encoding: 'utf-8' }));
 
 describe('Check team', function () {
   it('should return number people in team', function () {
-    team[1].should.have.property('teamHistory').with.lengthOf(8);
+    expect(team[1].teamHistory).to.have.length(8);
   });
 
   it('onduty and index 0 should be the same person', function () {
     const onduty = team[1].teamHistory[0].name;
     const index0 = team[0].onDuty;
-    assert.equal(onduty, index0);
+    expect(onduty).to.equal(index0);
   });
 
   it('onduty should not be the same person as backup', function () {
     const onduty = team[1].teamHistory[0].name;
     const backup = team[1].teamHistory[1].name;
-    onduty.should.not.equal(backup);
+    expect(onduty).not.to.equal(backup);
   });
 
   it('John should not exist in the team', function () {
     let johnDoe = team[1].teamHistory.find((person) => person.name === 'John Doe');
-    should.not.exist(johnDoe);
+    expect(johnDoe).not.to.exist;
   });
 
   it('should not return a null result', function () {
