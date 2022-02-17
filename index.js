20635ae47790994c21e373706b22b904052a9cc1
discombobulateme <paloma.oliveira@saucelabs.com>
2022-02-17 16:13:31 +0100

    Fix next date

--- index.js
+++ index.js
@@ -2,23 +2,25 @@ const fs = require('fs/promises');
 const path = require('path');
 const colors = require('ansi-colors');
 
-const team = require("./team.json");
+const team = require('./team.json');
 
 /* Checker to void rotation before due date */
 let currentDate = new Date();
 let oldNext = team[0].nextDate;
-let next = new Date (team[0].nextDate);
-if( currentDate <= next ) {
+let next = new Date(team[0].nextDate);
+
+//console.log(currentDate <= next ? true : false );
+if (currentDate <= next) {
   console.log(`
   Next duty date will be on ${colors.greenBright(oldNext)}
   On duty is: ${colors.magentaBright(team[1].teamHistory[0].name)}
   Backup is: ${colors.yellow(team[1].teamHistory[1].name)}
-  `)
-  process.exit()
+  `);
+  process.exit();
 }
 
 /* Find next date = every other week = 14 days interval */
-let lastDate = team[0].lastDate;
+let lastDate = team[0].nextDate;
 let last = new Date(lastDate);
 let nextDate = new Date(last.setDate(last.getDate() + 14));
 let nextReadableDate = nextDate.toDateString();
@@ -27,20 +29,16 @@ let nextReadableDate = nextDate.toDateString();
 /* Update last and next dates */
 team[0].nextDate = nextReadableDate;
 team[0].lastDate = oldNext;
-
 /* Write last onDuty and last backup to keep track */
 let lastOnDuty = team[1].teamHistory[0].name;
 let lastBackup = team[1].teamHistory[1].name;
-team[0].lastPeople = `Last onDuty: ${lastOnDuty}. Last backup: ${lastBackup}`
-
+team[0].lastPeople = `Last onDuty: ${lastOnDuty}. Last backup: ${lastBackup}`;
 /* Rotate team array positions */
 const teamArray = team[1].teamHistory;
 const rotate = teamArray.push(teamArray.shift());
 team[1].teamHistory = teamArray;
 
 /* Check if next position works, or needs to be redone */
-
-
 
 /* ONDUTY & BACKUP PERSON UPDATE */
 /* Find position 0 and write it as onDuty */
@@ -70,14 +68,13 @@ console.log(`
  */
 
 /* JSON.stringify(Object, format not to be in one line, spaces indentation) */
-fs.writeFile(path.join(__dirname, './team.json'), JSON.stringify(team, null, 2),
-  function writeJSON(err) {
-    if (err) return console.log(err);
-  });
+fs.writeFile(path.join(__dirname, './team.json'), JSON.stringify(team, null, 2), function writeJSON(err) {
+  if (err) return console.log(err);
+});
 
 module.exports = {
   slack: {
     signinSecret: process.env.SLACK_SIGNIN_SECRET,
-    token: process.env.SLACK_TOKEN
-  }
+    token: process.env.SLACK_TOKEN,
+  },
 };
